6bc63e2efa5fbbd23e077a4ca115a83217056683
jotchwali <122258840+jotchwali@users.noreply.github.com>
2024-03-18 04:04:30 +1300

    Update popup.js
    saving data

--- popup.js
+++ popup.js
@@ -1,1 +1,9 @@
-console.log("This is a popup!")
+// Save data to local storage
+function saveData(key, value) {
+  chrome.storage.local.set({ [key]: value }, function() {
+    console.log('Data saved:', key, value);
+  });
+}
+
+// Example usage:
+saveData('myKey', 'myValue');
